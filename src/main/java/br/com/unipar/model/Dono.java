07bb7bd8c7caf151bac4776d6dff112233064a35
Caio Lopes <caio.alves.lopes89@gmail.com>
2023-04-17 14:11:48 -0300

    Atualização dos metodos da classe dono e instânciamento dos objetos Dono e Cliente feitos na main

--- src/main/java/br/com/unipar/model/Dono.java
+++ src/main/java/br/com/unipar/model/Dono.java
@@ -6,21 +6,26 @@ import java.util.Date;
 public class Dono extends Pessoa {
     private ArrayList<Cliente> clientes;
 
-
+    //construtor
     public Dono(String nome, String cpf, int idade, String genero, String rg, String dataNascimento, Endereco endereco, int id, Date dataCadastro) {
         super(nome, cpf, idade, genero, rg, dataNascimento, endereco, id, dataCadastro);
-
+        this.clientes = new ArrayList<>();
     }
+
+    //metodo construtor vazio
+    public Dono(){}
 
     public Cliente criarCliente(String nome, String cpf, int idade, String genero, String rg, String dataNascimento, Endereco endereco, int id, Date dataCadastro, double dinheiro){
         Cliente cliente = new Cliente(nome, cpf, idade, genero, rg, dataNascimento, endereco, id, dataCadastro, dinheiro);
         return cliente;
     }
 
-    public void adicionarCliente(Cliente cliente){
+    // adicionar novos clientes
+    public void addCliente(Cliente cliente){
         this.clientes.add(cliente);
     }
 
+    //Buscar clientes pelo Id de cada um
     public Cliente buscarIdClientes(int id){
         for (Cliente cliente: clientes){
             if (cliente.getId() == id){
